a61904248601ab2877d283b89173a9c9033b9af2
Kam <kamran.programing@gmail.com>
2025-01-26 10:28:46 +0000

    Error Handeling, DTOs

---
+++ API/DTOs/CreateProductDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class CreateProductDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Range(0.1, double.MaxValue)]
+    public decimal Price { get; set; }
+    [Required]
+    public string PictureUrl { get; set; } = string.Empty;
+    [Required]
+    public string Type { get; set; } = string.Empty;
+    [Required]
+    public string Brand { get; set; } = string.Empty;
+    [Range(0, int.MaxValue,  ErrorMessage = "Quantity must be greater than 0")]
+    public int QuantityInStock { get; set; }
+}
